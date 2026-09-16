5ab38ca67e907ed968dbfcda6b542d88555389e3
AGU <48829902+AGUrGrill@users.noreply.github.com>
2026-09-15 20:32:04 -0400

    big dubs, runs but needs fixing

---
+++ RiskOfRuneProject/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BIGSHOT = 1506173320U;
+        static const AkUniqueID PLAY_DIALTONE = 2011881192U;
+        static const AkUniqueID PLAY_GRAVE = 3115073109U;
+        static const AkUniqueID PLAY_ICESHOCK = 893303731U;
+        static const AkUniqueID PLAY_ROARINGBLADE = 1166387236U;
+        static const AkUniqueID PLAY_RUDEBUSTER = 2204239051U;
+        static const AkUniqueID PLAY_SAGEHEAL = 520313368U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID VOLUME_SFX = 3673881719U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID RISKOFRUNESOUNDBANK = 2909651858U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
